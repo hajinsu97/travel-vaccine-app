5730d7cd7751344bcdabad8cd371375cdf8e006d
Jinsu Ha <jinsu.jh.ha@gmail.com>
2023-02-19 23:48:52 -0500

    add vaccine recommendations to response output

--- CDCScraper.py
+++ CDCScraper.py
@@ -2,9 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 from flask import abort
 
+from Vaccine import Vaccine
+
 URL = "https://wwwnc.cdc.gov/travel/destinations/traveler/none/"
 VACCINES_AND_MEDICINES_HTML_ID = "vaccines-and-medicines"
 CLINICIAN_DISEASES_HTML_CLASS_NAME = "clinician-disease"
+CLINICIAN_RECOMMENDATIONS_HTML_CLASS_NAME = "clinician-recomendations"
 
 
 def getVaccines(country: str) -> list[str]:
@@ -17,15 +20,22 @@ def getVaccines(country: str) -> list[str]:
     # Find the "Vaccines and Medicines" table
     soup = BeautifulSoup(page.content, "html.parser")
     vaccinesAndMedicinesTable = soup.find(id=VACCINES_AND_MEDICINES_HTML_ID)
-
-    # Find each entry for "Vaccine for disease" in the table
+    
+    # Get a list of each entry under "Vaccine for disease" in the table
     clinicianDiseases = vaccinesAndMedicinesTable.find_all(
         "td", class_=CLINICIAN_DISEASES_HTML_CLASS_NAME
     )
 
+    # Get a list of each entry under "Recommendations" in the table
+    clinicianRecommendations = vaccinesAndMedicinesTable.find_all(
+        "td", class_=CLINICIAN_RECOMMENDATIONS_HTML_CLASS_NAME
+    )
+
     # Find the disease name within each table cell
-    diseaseNames = []
-    for clinicianDisease in clinicianDiseases:
-        diseaseName = clinicianDisease.find("a").text
-        diseaseNames.append({"vaccine": diseaseName})
-    return diseaseNames
+    vaccinesList = []
+    for x in range(len(clinicianDiseases)):
+        diseaseName = clinicianDiseases[x].find("a").text
+        recommendation = clinicianRecommendations[x].find("p").text
+        vaccinesList.append(Vaccine(diseaseName, recommendation).__dict__)
+    # TODO: Missing Yellow fever recommendation
+    return vaccinesList
---
+++ Vaccine.py
@@ -0,0 +1,4 @@
+class Vaccine:
+    def __init__(self, disease, recommendations):
+        self.disease = disease
+        self.recommendations = recommendations
