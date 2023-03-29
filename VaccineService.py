56feead79c7033b5a7313789aed601bd3a9d9a3b
hajinsu97 <jinsu.jh.ha@gmail.com>
2023-03-28 21:36:01 -0400

    initial implementation of getDosage

--- VaccineService.py
+++ VaccineService.py
@@ -1,5 +1,26 @@
+import csv
 from model.Dosage import Dosage
-
+from model.Dose import Dose
 
 def getDosages(vaccine: str, dateOfBirth: str) -> list[Dosage]:
-    return []
+    dosageList = []
+    with open('Vaccination Logic table_03_26_2023.csv') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            dose = convertStringToDose(row["Dose"])
+            dosageList.append(Dosage(row["Generic"], row["Brand"], dose, row["Number of Doses"], row["Schedule (months)"]).__dict__)
+    return dosageList
+
+# Return a Dose given a string representing a dose e.g., "1.5 ml"
+def convertStringToDose(dose: str) -> Dose:
+    amount = None
+    unit = None
+    split = dose.split()
+
+    if (len(split) > 0):
+        amount = split[0]
+    if (len(split) > 1):
+        unit = split[1]
+
+    return Dose(amount, unit).__dict__
--- model/Age.py
+++ model/Age.py
@@ -1,4 +1,4 @@
 class Age:
-    def __init__(self, value, unit):
+    def __init__(self, value: int, unit: str):
         self.value = value
         self.unit = unit
