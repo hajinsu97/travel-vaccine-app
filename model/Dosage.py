e5bcc405cd38ebdb1e4eb5e2280ceb603a2b3a95
hajinsu97 <jinsu.jh.ha@gmail.com>
2023-03-30 22:01:14 -0400

    refactor dose to be string instead of Dose object

--- model/Dosage.py
+++ model/Dosage.py
@@ -1,10 +1,9 @@
 from model.Age import Age
-from model.Dose import Dose
 
 
 # TODO: Use a Builder Pattern?
 class Dosage:
-    def __init__(self, disease: str, genericName: str, brandName: str, form: str, dose: Dose, numberOfDoses: int, schedule: str):
+    def __init__(self, disease: str, genericName: str, brandName: str, form: str, dose: str, numberOfDoses: int, schedule: str):
         self.disease = disease
         self.genericName = genericName
         self.brandName = brandName
--- model/Dose.py
+++
@@ -1,4 +0,0 @@
-class Dose:
-    def __init__(self, value: float, unit: str):
-        self.value = value
-        self.unit = unit
