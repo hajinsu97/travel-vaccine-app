540c22caa6dcb73d03edcdae0822d2fdb8f8124c
hajinsu97 <jinsu.jh.ha@gmail.com>
2023-03-30 21:52:46 -0400

    add form and disease to Get Dosage response body

--- model/Dosage.py
+++ model/Dosage.py
@@ -2,10 +2,13 @@ from model.Age import Age
 from model.Dose import Dose
 
 
+# TODO: Use a Builder Pattern?
 class Dosage:
-    def __init__(self, genericName: str, brandName: str, dose: Dose, numberOfDoses: int, schedule: str):
+    def __init__(self, disease: str, genericName: str, brandName: str, form: str, dose: Dose, numberOfDoses: int, schedule: str):
+        self.disease = disease
         self.genericName = genericName
         self.brandName = brandName
+        self.form = form
         # self.minAge = minAge
         # self.maxAge = maxAge
         self.dose = dose
