05f03f9ee71620806e5280da0c4c452973e182a6
hajinsu97 <jinsu.jh.ha@gmail.com>
2023-03-30 22:07:16 -0400

    rename constants for column headers

--- service/VaccineDosageService.py
+++ service/VaccineDosageService.py
@@ -2,13 +2,13 @@ import csv
 from model.Dosage import Dosage
 
 VACCINE_LOGIC_TABLE_FILE_NAME = "Vaccination Logic table.csv"
-VACCINE_LOGIC_TABLE_COLUMN_DISEASE = "Disease/Illness"
-VACCINE_LOGIC_TABLE_COLUMN_BRAND_NAME = "Brand"
-VACCINE_LOGIC_TABLE_COLUMN_FORM = "Form"
-VACCINE_LOGIC_TABLE_COLUMN_DOSE = "Dose"
-VACCINE_LOGIC_TABLE_COLUMN_GENERIC_NAME = "Generic"
-VACCINE_LOGIC_TABLE_COLUMN_NUMBER_OF_DOSES = "Number of Doses"
-VACCINE_LOGIC_TABLE_COLUMN_SCHEDULE_IN_MONTHS = "Schedule (months)"
+VACCINE_LOGIC_TABLE_COLUMN_HEADER_DISEASE = "Disease/Illness"
+VACCINE_LOGIC_TABLE_COLUMN_HEADER_BRAND_NAME = "Brand"
+VACCINE_LOGIC_TABLE_COLUMN_HEADER_FORM = "Form"
+VACCINE_LOGIC_TABLE_COLUMN_HEADER_DOSE = "Dose"
+VACCINE_LOGIC_TABLE_COLUMN_HEADER_GENERIC_NAME = "Generic"
+VACCINE_LOGIC_TABLE_COLUMN_HEADER_NUMBER_OF_DOSES = "Number of Doses"
+VACCINE_LOGIC_TABLE_COLUMN_HEADER_SCHEDULE_IN_MONTHS = "Schedule (months)"
 
 def getDosages(vaccine: str, dateOfBirth: str) -> list[Dosage]:
     dosageList = []
@@ -17,12 +17,12 @@ def getDosages(vaccine: str, dateOfBirth: str) -> list[Dosage]:
         line_count = 0
         for row in csv_reader:
             # TODO: Make constants file?
-            dosage = Dosage(row[VACCINE_LOGIC_TABLE_COLUMN_DISEASE],
-                            row[VACCINE_LOGIC_TABLE_COLUMN_GENERIC_NAME], 
-                            row[VACCINE_LOGIC_TABLE_COLUMN_BRAND_NAME], 
-                            row[VACCINE_LOGIC_TABLE_COLUMN_FORM],
-                            row[VACCINE_LOGIC_TABLE_COLUMN_DOSE], 
-                            row[VACCINE_LOGIC_TABLE_COLUMN_NUMBER_OF_DOSES], 
-                            row[VACCINE_LOGIC_TABLE_COLUMN_SCHEDULE_IN_MONTHS])
+            dosage = Dosage(row[VACCINE_LOGIC_TABLE_COLUMN_HEADER_DISEASE],
+                            row[VACCINE_LOGIC_TABLE_COLUMN_HEADER_GENERIC_NAME], 
+                            row[VACCINE_LOGIC_TABLE_COLUMN_HEADER_BRAND_NAME], 
+                            row[VACCINE_LOGIC_TABLE_COLUMN_HEADER_FORM],
+                            row[VACCINE_LOGIC_TABLE_COLUMN_HEADER_DOSE], 
+                            row[VACCINE_LOGIC_TABLE_COLUMN_HEADER_NUMBER_OF_DOSES], 
+                            row[VACCINE_LOGIC_TABLE_COLUMN_HEADER_SCHEDULE_IN_MONTHS])
             dosageList.append(dosage.__dict__)
     return dosageList
