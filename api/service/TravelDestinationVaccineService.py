7855e9c031f6f3dee279e7091f5596740ab6d730
hajinsu97 <jinsu.jh.ha@gmail.com>
2023-05-27 22:44:51 -0400

    implement getDestinations

--- api/service/TravelDestinationVaccineService.py
+++ api/service/TravelDestinationVaccineService.py
@@ -5,14 +5,37 @@ from flask import abort
 from model.Vaccine import Vaccine
 from model.VaccinesList import VaccinesList
 
-URL = "https://wwwnc.cdc.gov/travel/destinations/traveler/none/"
+DESTINATIONS_LIST_URL = "https://wwwnc.cdc.gov/travel/destinations/list"
+DESTINATIONS_SELECT_LIST_ID = "thlrdssl-traveler"
+
+# TODO: Unit test getDestinations()
+def getDestinations() -> list[str]:
+    page = requests.get(DESTINATIONS_LIST_URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    country_list = []
+
+    # Find all <ul> elements containing the countries
+    ul_elements = soup.find_all('ul', class_='list-bullet')
+    for ul_element in ul_elements:
+        # Find all <li> elements within the <ul>
+        li_elements = ul_element.find_all('li')
+
+        # Extract the country names from the <a> elements within the <li> elements
+        for li in li_elements:
+            destination_url_path = li.find('a').get('href')
+            country_name = destination_url_path.split("/")[-1]
+            country_list.append(country_name)
+
+    return country_list
+
+# TODO: Unit test for getVaccines() (1) Existing country (2) Non-existing country
+DESTINATIONS_URL_PREFIX = "https://wwwnc.cdc.gov/travel/destinations/traveler/none/"
 VACCINES_AND_MEDICINES_HTML_ID = "vaccines-and-medicines"
 CLINICIAN_DISEASES_HTML_CLASS_NAME = "clinician-disease"
 CLINICIAN_RECOMMENDATIONS_HTML_CLASS_NAME = "clinician-recomendations"
 
-
 def getVaccines(country: str) -> VaccinesList:
-    destinationUrl = URL + country
+    destinationUrl = DESTINATIONS_URL_PREFIX + country
     page = requests.get(destinationUrl)
 
     if page.status_code == 404:
