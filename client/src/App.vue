0aa6b98ee8eef59734ef2a7f97fccd15e86a5c7b
hajinsu97 <jinsu.jh.ha@gmail.com>
2023-05-28 23:10:51 -0400

    use destination.displayName in list and Id for api call

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,15 +5,15 @@
       <h3>✈️ Where are you travelling to?</h3>
       <select class="select-country" v-model="selectedCountry" @change="handleCountrySelected">
         <option disabled value="">Please select a country</option>
-        <option v-for="destination in destinations" :value="destination">{{ destination }}</option>
+        <option v-for="destination in destinations" :value="destination">{{ destination.displayName }}</option>
       </select>
     </div>
     
     <div v-if="selectedCountry" class="vaccine-info">
       <h2>💉 Vaccine Information</h2>
-      <a :href="vaccineInfo.link" target="_blank">{{ selectedCountry }} on CDC website</a>
+      <a :href="vaccineInfo.link" target="_blank">{{ selectedCountry.displayName }} on CDC website</a>
       <ul>
-        <li v-for="vaccine in vaccineInfo.vaccines" :key="vaccine.id">
+        <li v-for="vaccine in vaccineInfo.items" :key="vaccine.id">
           <p class="vaccine-disease">Vaccine: {{ vaccine.disease }}</p>
           <p class="vaccine-recommendations">Recommendations: {{ vaccine.recommendations }}</p>
         </li>
@@ -48,7 +48,7 @@ import { RouterView } from 'vue-router'
       },
       async getVaccines() {
         try {
-          const response = await axios.get(`/api/destinations/${this.selectedCountry}/vaccines`);
+          const response = await axios.get(`/api/destinations/${this.selectedCountry.id}/vaccines`);
           this.vaccineInfo = response.data;
           console.log('Got vaccine info:', this.vaccineInfo)
         } catch (error) {
