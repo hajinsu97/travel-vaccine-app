cd5cdd0cb5e6d34532e746065ed611ac42131d67
hajinsu97 <jinsu.jh.ha@gmail.com>
2023-05-28 22:25:48 -0400

    add vaccine info

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,20 +2,20 @@
   <RouterView />
   <div id="app">
     <div class="country-dropdown">
-      <select v-model="selectedCountry" @change="handleCountrySelected">
+      <select class="select-country" v-model="selectedCountry" @change="handleCountrySelected">
         <option disabled value="">Please select a country</option>
         <option v-for="destination in destinations" :value="destination">{{ destination }}</option>
       </select>
     </div>
     <p>Selected country: {{ selectedCountry }}</p>
     
-    <div v-if="selectedCountry">
+    <div v-if="selectedCountry" class="vaccine-info">
       <h2>Vaccine Information</h2>
-      <a :href="vaccineInfo.link" target="_blank">Vaccine information for {{ this.selectedCountry }} on CDC website</a>
+      <a :href="vaccineInfo.link" target="_blank">Vaccine information for {{ selectedCountry }} on CDC website</a>
       <ul>
         <li v-for="vaccine in vaccineInfo.vaccines" :key="vaccine.id">
-          <p>Vaccine: {{ vaccine.disease }}</p>
-          <p>Doses: {{ vaccine.recomendations }}</p>
+          <p class="vaccine-disease">Vaccine: {{ vaccine.disease }}</p>
+          <p class="vaccine-recommendations">Recommendations: {{ vaccine.recommendations }}</p>
         </li>
       </ul>
     </div>
