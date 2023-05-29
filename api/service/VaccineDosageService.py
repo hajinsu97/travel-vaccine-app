2c299bcd31078db86f80bf68feb85e21134c3ea5
hajinsu97 <jinsu.jh.ha@gmail.com>
2023-05-29 16:41:21 -0400

    fix list is not subscriptable

--- api/service/VaccineDosageService.py
+++ api/service/VaccineDosageService.py
@@ -1,4 +1,5 @@
 import csv
+from typing import List
 from model.Dosage import Dosage
 
 VACCINE_LOGIC_TABLE_FILE_NAME = "Vaccination Logic table.csv"
@@ -10,7 +11,7 @@ VACCINE_LOGIC_TABLE_COLUMN_HEADER_GENERIC_NAME = "Generic"
 VACCINE_LOGIC_TABLE_COLUMN_HEADER_NUMBER_OF_DOSES = "Number of Doses"
 VACCINE_LOGIC_TABLE_COLUMN_HEADER_SCHEDULE_IN_MONTHS = "Schedule (months)"
 
-def getDosages(disease: str, dateOfBirth: str) -> list[Dosage]:
+def getDosages(disease: str, dateOfBirth: str) -> List[Dosage]:
     dosageList = []
     with open(VACCINE_LOGIC_TABLE_FILE_NAME) as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=',')
