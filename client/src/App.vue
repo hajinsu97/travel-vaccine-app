02761292f723a9d48f54e92069b85c1c12098217
hajinsu97 <70655743+hajinsu97@users.noreply.github.com>
2023-05-28 22:26:47 -0400

    [UI] Vaccine Info for a destination (#4)
    * initial output of getVaccines api
    
    * add vaccine info

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <RouterView />
   <div id="app">
-    <div class="dropdown">
-      <select v-model="selectedCountry" @change="handleCountrySelected">
+    <div class="country-dropdown">
+      <select class="select-country" v-model="selectedCountry" @change="handleCountrySelected">
         <option disabled value="">Please select a country</option>
         <option v-for="destination in destinations" :value="destination">{{ destination }}</option>
       </select>
     </div>
     <p>Selected country: {{ selectedCountry }}</p>
+    
+    <div v-if="selectedCountry" class="vaccine-info">
+      <h2>Vaccine Information</h2>
+      <a :href="vaccineInfo.link" target="_blank">Vaccine information for {{ selectedCountry }} on CDC website</a>
+      <ul>
+        <li v-for="vaccine in vaccineInfo.vaccines" :key="vaccine.id">
+          <p class="vaccine-disease">Vaccine: {{ vaccine.disease }}</p>
+          <p class="vaccine-recommendations">Recommendations: {{ vaccine.recommendations }}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -19,8 +30,9 @@ import { RouterView } from 'vue-router'
   export default {
     data() {
       return {
-        destinations: '',
-        selectedCountry: 'Where are you travelling to?'
+        selectedCountry: 'Where are you travelling to?',
+        destinations: [],
+        vaccineInfo: {},
       };
     },
     methods: {
@@ -31,12 +43,23 @@ import { RouterView } from 'vue-router'
             this.destinations = res.data;
           })
           .catch((error) => {
-            console.error(error);
+            console.error('Error fetching destinations list:', error);
           });
       },
-      handleCountryChange() {
-        console.log('Selected country:', this.selectedCountry);
+      async getVaccines() {
+        try {
+          const response = await axios.get(`/api/destinations/${this.selectedCountry}/vaccines`);
+          this.vaccineInfo = response.data;
+          console.log('Got vaccine info:', this.vaccineInfo)
+        } catch (error) {
+          console.error('Error fetching vaccine information:', error);
+        }
       },
+      handleCountrySelected() {
+        console.log('Selected country:', this.selectedCountry);
+        this.getVaccines()
+      },
+
     },
     created() {
       this.getDestinations();
