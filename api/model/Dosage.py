1ec5cc01828c041ae11b8657dfaeeafcd8785315
hajinsu97 <jinsu.jh.ha@gmail.com>
2023-06-25 16:25:50 -0400

    add min/max age to Dosage object

--- api/model/Dosage.py
+++ api/model/Dosage.py
@@ -10,12 +10,14 @@ class Dosage:
         dose: str,
         number_of_doses: int,
         schedule: str,
+        min_age: Age,
+        max_age: Age,
     ):
         self.generic_name = generic_name
         self.brand_name = brand_name
         self.form = form
-        # self.min_age = min_age
-        # self.max_age = max_age
         self.dose = dose
         self.number_of_doses = number_of_doses
         self.schedule = schedule
+        self.min_age = min_age
+        self.max_age = max_age
