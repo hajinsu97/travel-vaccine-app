0e7e992c98025c9415ac222312ba80b432b6c8e2
hajinsu97 <jinsu.jh.ha@gmail.com>
2023-06-25 23:01:02 -0400

    add tests for current get_dosages behaviour

--- api/service/VaccineDosageService.py
+++ api/service/VaccineDosageService.py
@@ -30,7 +30,7 @@ def get_dosages(disease: str, csv_file_name=VACCINE_LOGIC_TABLE_FILE_NAME) -> Do
                     brand_name=row[BRAND_NAME_COLUMN_HEADER],
                     form=row[FORM_COLUMN_HEADER],
                     dose=row[DOSE_COLUMN_HEADER],
-                    number_of_doses = 0 if row[NUMBER_OF_DOSES_COLUMN_HEADER] == '' else int(row[NUMBER_OF_DOSES_COLUMN_HEADER]),
+                    number_of_doses = 0 if not row[NUMBER_OF_DOSES_COLUMN_HEADER] else int(row[NUMBER_OF_DOSES_COLUMN_HEADER]),
                     schedule=row[SCHEDULE_COLUMN_HEADER],
                 )
                 dosage_list.append(dosage.__dict__)
--- api/tests/test_VaccineDosageService.py
+++ api/tests/test_VaccineDosageService.py
@@ -3,6 +3,15 @@ import pytest
 from service.VaccineDosageService import get_dosages
 
 TEST_VACCINATION_LOGIC_TABLE_CSV_FILE = "tests/fixtures/Test_Vaccination_Logic_table.csv"
+DISEASE = "disease"
+ITEMS = "items"
+GENERIC_NAME = "generic_name"
+BRAND_NAME = "brand_name"
+FORM = "form"
+DOSE = "dose"
+NUMBER_OF_DOSES = "number_of_doses"
+SCHEDULE = "schedule"
+
 
 def _get_dosages(disease):
     """
@@ -10,34 +19,43 @@ def _get_dosages(disease):
     """
     return get_dosages(disease, csv_file_name=TEST_VACCINATION_LOGIC_TABLE_CSV_FILE)
 
-def test_get_dosages_non_existent_disease_returns_dosage_list_with_empty_items():
+def test_get_dosages_non_existent_disease_returns_0_dosages():
     non_existent_disease = "non existent disease"
     dosages = _get_dosages(non_existent_disease)
-    assert dosages["disease"] == non_existent_disease
-    assert dosages["items"] == []
+    assert dosages[DISEASE] == non_existent_disease
+    assert dosages[ITEMS] == []
 
-def test_get_dosages_valid_disease_name_returns_correct_dosage_list():
+def test_get_dosages_valid_disease_name_returns_correct_dosages():
     valid_disease_name = "Hepatitis A"
     dosages = _get_dosages(valid_disease_name)
-    assert valid_disease_name == dosages["disease"]
-    assert len(dosages["items"]) == 2
+    assert valid_disease_name == dosages[DISEASE]
+    assert len(dosages[ITEMS]) == 2
 
-    dosage_1 = dosages["items"][0]
-    assert dosage_1["generic_name"] == "Hepatitis A vaccine, inactivated"
-    assert dosage_1["brand_name"] == "Avaxim"
-    assert dosage_1["form"] == "Injectable"
-    assert dosage_1["dose"] == "0.5 mL"
-    assert dosage_1["number_of_doses"] == 2
-    assert dosage_1["schedule"] == "0, 3-36"
+    dosage_1 = dosages[ITEMS][0]
+    assert dosage_1[GENERIC_NAME] == "Hepatitis A vaccine, inactivated"
+    assert dosage_1[BRAND_NAME] == "Avaxim"
+    assert dosage_1[FORM] == "Injectable"
+    assert dosage_1[DOSE] == "0.5 mL"
+    assert dosage_1[NUMBER_OF_DOSES] == 2
+    assert dosage_1[SCHEDULE] == "0, 3-36"
 
-    dosage_2 = dosages["items"][1]
-    assert dosage_2["generic_name"] == "Hepatitis A vaccine, inactivated"
-    assert dosage_2["brand_name"] == "Havrix"
-    assert dosage_2["form"] == "Injectable"
-    assert dosage_2["dose"] == "1 mL"
-    assert dosage_2["number_of_doses"] == 2
-    assert dosage_2["schedule"] == "0, 6-12"
+    dosage_2 = dosages[ITEMS][1]
+    assert dosage_2[GENERIC_NAME] == "Hepatitis A vaccine, inactivated"
+    assert dosage_2[BRAND_NAME] == "Havrix"
+    assert dosage_2[FORM] == "Injectable"
+    assert dosage_2[DOSE] == "1 mL"
+    assert dosage_2[NUMBER_OF_DOSES] == 2
+    assert dosage_2[SCHEDULE] == "0, 6-12"
 
+def test_get_dosages_disease_name_is_case_insensitive():
+    assert _get_dosages("hepatitis b")[ITEMS] == _get_dosages("HEPATITIS B")[ITEMS]
 
+def test_get_dosages_pass_disease_name_in_snake_case_returns_dosages():
+    disease_name = 'hepatitis-b'
+    dosages = _get_dosages(disease_name)
+    assert dosages[DISEASE] == disease_name
+    assert len(dosages[ITEMS]) == 1
 
-
+def test_get_dosages_for_vaccine_with_empty_number_of_doses_returns_0_doses():
+    dosages = _get_dosages('hepatitis b')
+    assert dosages[ITEMS][0][NUMBER_OF_DOSES] == 0
